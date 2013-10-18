e5d93da81b9979f719fadedf4ded230deacddcf2
Olle Johansson <olle@avidity.se>
2013-10-18 15:20:59 +0200

    Initial setup of rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Spicyham::Application.config.secret_key_base = '4ade145430dc8fc802c45f71f8d281d4d298c3ab63ced0019b41f6fd5fe0f20899f9b8350d7f0dc35898f90dea7d9652fdddf62e5354b9ea112b414a82bd94b7'
