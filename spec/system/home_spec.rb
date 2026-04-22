78d5c37a64dd81a54f71e1cfdfc592b52c5e35fc
Olle Wreede <olle.wreede@medovia.se>
2026-04-22 19:51:04 +0200

    Improve landing page with tailwind components

--- spec/system/home_spec.rb
+++ spec/system/home_spec.rb
@@ -1,24 +1,52 @@
 require 'rails_helper'
 
 RSpec.describe "Home", type: :system do
-  it "shows landing page with feature descriptions" do
-    visit root_path
-    expect(page).to have_content("Email alias creation")
-    expect(page).to have_content("Increased security")
-    expect(page).to have_content("Spam protection")
-    expect(page).to have_content("Improved privacy")
-    expect(page).to have_content("Supported email providers")
+  context "unauthenticated" do
+    it "shows hero section with feature descriptions" do
+      visit home_path
+      expect(page).to have_content("Email alias creation")
+      expect(page).to have_content("Increased security")
+      expect(page).to have_content("Spam protection")
+      expect(page).to have_content("Improved privacy")
+    end
+
+    it "shows provider logo cloud" do
+      visit home_path
+      expect(page).to have_content(/works with/i)
+      expect(page).to have_css("img[alt='Migadu']")
+      expect(page).to have_css("img[alt='Gandi']")
+      expect(page).to have_css("img[alt='Glesys']")
+    end
+
+    it "shows Get started CTA in hero linking to sign-in" do
+      visit home_path
+      click_link "Get started"
+      expect(page).to have_current_path(new_user_session_path)
+    end
+
+    it "shows CTA section with Sign in button" do
+      visit home_path
+      expect(page).to have_content("Ready to protect your email?")
+      expect(page).to have_link("Sign in")
+    end
   end
 
-  it "has sign-in link" do
-    visit root_path
-    expect(page).to have_link("Sign in")
-  end
+  context "authenticated" do
+    before do
+      sign_in create(:user)
+      allow(Facade::API).to receive(:create).and_return(double("Facade::API", list: []))
+    end
 
-  it "navigates to sign-in page when clicking sign-in" do
-    visit root_path
-    click_link "Sign in"
-    expect(page).to have_current_path(new_user_session_path)
+    it "shows Go to Emails CTA in hero" do
+      visit home_path
+      click_link "Go to Emails"
+      expect(page).to have_content("Email forwards")
+    end
+
+    it "does not show the unauthenticated CTA section" do
+      visit home_path
+      expect(page).not_to have_content("Ready to protect your email?")
+    end
   end
 
   it "has OAuth sign-in button as a real form POST with Turbo disabled" do
