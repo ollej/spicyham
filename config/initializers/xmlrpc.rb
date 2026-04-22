78c435c53b637c4cd7982a8eab57e9837d82e5d3
Olle Wreede <olle.wreede@medovia.se>
2026-04-22 22:57:39 +0200

    Cleanup xmlrpc initializer

--- config/initializers/xmlrpc.rb
+++ config/initializers/xmlrpc.rb
@@ -1,7 +1,4 @@
 require 'xmlrpc/client'
 
-verbose = $VERBOSE
-$VERBOSE = nil
-XMLRPC::Config.const_set(:ENABLE_NIL_PARSER, true)
-XMLRPC::Config.const_set(:ENABLE_BIGINT, true)
-$VERBOSE = verbose
+XMLRPC::Config.enable_nil_parser = true
+XMLRPC::Config.enable_bigint = true
